8271e783a0c77068945a588fb546d7783059081a
Woodbin <woodbin@centrum.cz>
2014-10-22 19:12:32 +0200

    ControlScript

--- Assets/Scripts/ControlScript.cs
+++ Assets/Scripts/ControlScript.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class ControlScript : MonoBehaviour {
+	public double step = 0.5;
+	public double maxSpeed = 1;
+	private float movex=0;
+	private float movey=0;
+	private Vector3 pos;
+	private Transform tr;
 
 	// Use this for initialization
 	void Start () {
@@ -12,4 +18,33 @@ public class ControlScript : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void FixedUpdate(){
+		if(Input.GetAxis("Horizontal")>0) movex= Mathf.Ceil(Input.GetAxis("Horizontal"));
+		else 		movex= Mathf.Floor(Input.GetAxis("Horizontal"));
+		if(Input.GetAxis("Vertical")>0) movey= Mathf.Ceil(Input.GetAxis("Vertical"));
+		else 		movey= Mathf.Floor(Input.GetAxis("Vertical"));
+		
+		
+		
+		if(tr.position==pos){
+			//pos=tr.position;
+			if(movex>0&&movey==0){
+				pos+=Vector3.right*step;
+				
+			}else if(movex<0&&movey==0){
+				pos+=Vector3.left*step;
+				
+			}else if(movey>0&&movex==0){
+				pos+=Vector3.up*step;
+				
+			}else if(movey<0&&movex==0){
+				pos+=Vector3.down*step;
+				
+			}
+			
+		}
+		transform.position= Vector3.MoveTowards(transform.position,pos, Time.deltaTime*maxSpeed);
+
+
 }
