f9c8afb1c134ea211c6561dfc5fd3d6db88ce991
Woodbin <woodbin@centrum.cz>
2014-10-30 17:49:44 +0100

    Remade BugControlScript
    Complete rewrite.

--- Assets/Scripts/BugControlScript.cs
+++ Assets/Scripts/BugControlScript.cs
@@ -2,43 +2,73 @@
 using System.Collections;
 
 public class BugControlScript : MonoBehaviour {
-	public float step = 16;
-	public float maxSpeed = 32;
-	private float movex=0;
-	private float movey=0;
-	private Vector3 pos;
-	private Transform tr;
+	//public float step = 16;
+	private float moveSpeed = 32f;
+	private float gridSize = 1f;
+	private enum Orientation { Horizontal, Vertical};
+	private Orientation gridOrientation = Orientation.Horizontal;
+	private bool allowDiagonals = false;
+	private bool correctDiagonalSpeed = true;
+	private Vector2 input;
+	private bool isMoving = false;
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private float t;
+	private float factor;
+	//public float rayCastLength = 16;
+/*	private float movex=0;
+	private float movey=0;*/
+//	private Vector3 pos;
+	//private Transform tr;
+	public bool rayState;
 
 	// Use this for initialization
 	void Start () {
-		tr = gameObject.transform;
-		pos = transform.position;
+	/*	tr = gameObject.transform;
+		pos = transform.position;*/
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		if(!isMoving){
+			input = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+			bool ray = false;
+			if(input.x>0&&input.y==0) ray = Level_Control.prediction(pos.x,pos.y,0);
+			if(input.x<0&&input.y==0) ray = Level_Control.prediction(pos.x,pos.y,1);
+			if(input.y>0&&input.x==0) ray = Level_Control.prediction(pos.x,pos.y,2);
+			if(input.y<0&&input.x==0) ray = Level_Control.prediction(pos.x,pos.y,3);
+			rayState = ray;
+			if(!allowDiagonals){
 
+				if(Mathf.Abs(input.x) > Mathf.Abs(input.y)){
+					input.y=0;
+				}else{
+					input.x=0;				
+				}
+			}
+			if(input!= Vector2.zero){
+				if(!ray)StartCoroutine(move(transform));
+			}
+		
+
+		}
+		gameObject.GetComponent<Animator>().SetFloat("speed",Mathf.Abs(input.x+input.y));
+
+	}
+	/*
 	void FixedUpdate(){
-		if(Input.GetAxis("Horizontal")>0) movex= Mathf.Ceil(Input.GetAxis("Horizontal"));
+		/*if(Input.GetAxis("Horizontal")>0) movex= Mathf.Ceil(Input.GetAxis("Horizontal"));
 		else 		movex= Mathf.Floor(Input.GetAxis("Horizontal"));
 		if(Input.GetAxis("Vertical")>0) movey= Mathf.Ceil(Input.GetAxis("Vertical"));
 		else 		movey= Mathf.Floor(Input.GetAxis("Vertical"));
 		gameObject.GetComponent<Animator>().SetFloat("speed",0);
-
+		Vector3 oldPos = new Vector3(pos.x,pos.y,pos.z);
 		
-		bool ray = true;
-		if(movex>0&&movey==0) ray = Physics.Raycast(pos,Vector3.right,4);
-		if(movex<0&&movey==0) ray = Physics.Raycast(pos,Vector3.left,4);
-		if(movey>0&&movex==0) ray = Physics.Raycast(pos,Vector3.up,4);
-		if(movey<0&&movex==0) ray = Physics.Raycast(pos,Vector3.down,4);
 
-		if((tr.position==pos)&&!ray){
+		if((tr.position==pos)){
 			gameObject.GetComponent<Animator>().SetFloat("speed",Mathf.Abs(0));
 
-			//pos=tr.position;
 			if(movex>0&&movey==0){
 				pos+=Vector3.right*step;
 				
@@ -54,15 +84,49 @@ public class BugControlScript : MonoBehaviour {
 					pos+=Vector3.down*step;
 				
 			}
-			
 		}
-		transform.position= Vector3.MoveTowards(transform.position,pos, Time.deltaTime*maxSpeed);
-		gameObject.GetComponent<Animator>().SetFloat("speed",Mathf.Abs(movex+movey));
+		if(ray) pos=oldPos;
+		transform.position= Vector3.MoveTowards(transform.position,pos, Time.deltaTime*moveSpeed);
 
+		
+	}*/
 
-
+	public IEnumerator move(Transform transform){
+		isMoving = true;
+		startPosition = transform.position;
+		t = 0;
+		
+		if(gridOrientation == Orientation.Horizontal) {
+			endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+			                          startPosition.y, startPosition.z + System.Math.Sign(input.y) * gridSize);
+		} else {
+			endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+			                          startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
+		}
+		
+		if(allowDiagonals && correctDiagonalSpeed && input.x != 0 && input.y != 0) {
+			factor = 0.7071f;
+		} else {
+			factor = 1f;
+		}
+		
+		while (t < 1f) {
+			t += Time.deltaTime * (moveSpeed/gridSize) * factor;
+			transform.position = Vector3.Lerp(startPosition, endPosition, t);
+			yield return null;
+		}
+		
+		isMoving = false;
 
 }
+/*	void OnCollisionEnter(Collision c){
+		if(movex!=0) transform.
+		if(movey!=0) 
+	
+	
+	}*/
+
+
 
 	public void setPosition(float x, float y){
 		gameObject.transform.Translate(x,y,0);
