e5c204473b507f9be5b37aebf427a8d4cfa06e3a
Woodbin <woodbin@centrum.cz>
2014-11-27 16:57:43 +0100

    added barrelcontrol with basic functionality

---
+++ Assets/Scripts/BarrelControl.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelControl : MonoBehaviour {
+
+    private string levelName;
+    private Level_Control lc;
+    private Vector3 pos;
+    private Transform tr;
+
+    //grid movement variables
+    private float moveSpeed = 96f;
+    private float gridSize = 32f;
+    private enum Orientation { Horizontal, Vertical };
+    private Orientation gridOrientation = Orientation.Horizontal;
+    private bool allowDiagonals = false;
+    private bool correctDiagonalSpeed = true;
+    private Vector2 input;
+    private bool isMoving = false;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float t;
+    private float factor;
+
+	// Use this for initialization
+	void Start () {
+        levelName = "mapa_pro_pohyb";
+        lc = GameObject.Find(levelName).GetComponent<Level_Control>();
+        tr = gameObject.transform;
+        pos = transform.position;
+	}
+
+
+    public IEnumerator move(Transform transform)
+    {
+        isMoving = true;
+        startPosition = transform.position;
+        t = 0;
+
+        if (gridOrientation == Orientation.Horizontal)
+        {
+            endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+                                      startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
+        }
+        else
+        {
+            endPosition = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize,
+                                      startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
+        }
+
+        if (allowDiagonals && correctDiagonalSpeed && input.x != 0 && input.y != 0)
+        {
+            factor = 0.7071f;
+        }
+        else
+        {
+            factor = 1f;
+        }
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * (moveSpeed / gridSize) * factor;
+            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+            yield return null;
+        }
+
+        isMoving = false;
+
+    }
+
+
+
+
+    bool push(int to)
+    {
+        bool success = false;
+
+        if (!lc.prediction(pos.x, pos.y, to))
+        {
+            StartCoroutine(move(transform));
+            success = true;
+        }
+        return success;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
